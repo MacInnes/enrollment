fc0fad969d968dca89b39f54aba1e03ef31ba609
MacInnes <califormula@gmail.com>
2018-07-10 14:39:18 -0600

    ok NOW all nav is passing tests

--- spec/features/students/nav_works_spec.rb
+++ spec/features/students/nav_works_spec.rb
@@ -1,34 +1,48 @@
 require 'rails_helper'
 
 describe 'Navigation' do
-  it 'can navigate to see all students from student show page' do
-    student = Student.create(name: "Andrew")
+  context 'All students link' do
+    it 'can navigate to see all students from student show page' do
+      student = Student.create(name: "Andrew")
 
-    visit student_path(student)
-    link_name = "Students"
+      visit student_path(student)
+      link_name = "Students"
 
-    expect(page).to have_content(link_name)
-    click_on(link_name)
-    expect(current_path).to eq(students_path)
+      expect(page).to have_content(link_name)
+      click_on(link_name)
+      expect(current_path).to eq(students_path)
+    end
+
+    it 'can navigate to see all students from student edit page' do
+      student = Student.create(name: "Andrew")
+
+      visit edit_student_path(student)
+      link_name = "Students"
+
+      expect(page).to have_content(link_name)
+      click_on(link_name)
+      expect(current_path).to eq(students_path)
+    end
+
+    it 'can navigate to see all students from student new page' do
+      visit new_student_path
+      link_name = "Students"
+
+      expect(page).to have_content(link_name)
+      click_on(link_name)
+      expect(current_path).to eq(students_path)
+    end
   end
 
-  it 'can navigate to see all students from student edit page' do
-    student = Student.create(name: "Andrew")
+  context 'One student link' do
+    it 'can view a single student from students page' do
+      student = Student.create(name: "Andrew")
 
-    visit edit_student_path(student)
-    link_name = "Students"
+      visit students_path
 
-    expect(page).to have_content(link_name)
-    click_on(link_name)
-    expect(current_path).to eq(students_path)
+      click_on("Andrew")
+      expect(current_path).to eq(student_path(student))
+    end
   end
 
-  it 'can navigate to see all students from student new page' do
-    visit new_student_path
-    link_name = "Students"
-
-    expect(page).to have_content(link_name)
-    click_on(link_name)
-    expect(current_path).to eq(students_path)
-  end
 end
